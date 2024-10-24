6fcd992a5e836b46e1b948fe355b1b637998200c
Yorozuya <yorozuya@Yorozuya-Mac.local>
2024-10-25 02:36:59 +0700

    api refresh token

--- app/api/refresh_token/route.ts
+++ app/api/refresh_token/route.ts
@@ -1,7 +1,7 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse, NextApiHandler } from 'next';
 import { refreshAllTokens } from '@/app/services/useTokenRefresh';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+const handler: NextApiHandler = async (req, res) => {
   if (req.method === 'POST') {
     try {
       await refreshAllTokens();
@@ -13,4 +13,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
-}
+};
+
+export default handler;
