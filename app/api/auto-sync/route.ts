f7b569483d2b722b953a85d5417600c869cf5b42
Yorozuya <yorozuya@Yorozuya-Mac.local>
2024-12-12 00:25:51 +0700

    sinc

--- app/api/auto-sync/route.ts
+++ app/api/auto-sync/route.ts
@@ -2,6 +2,9 @@ import { NextRequest, NextResponse } from 'next/server';
 import { syncOrders } from '@/app/services/orderSyncs';
 import { getAllShops } from '@/app/services/shopeeService';
 
+export const dynamic = 'force-dynamic';
+export const runtime = 'edge';
+
 export async function GET(request: NextRequest) {
   try {
     console.log('Memulai sinkronisasi...');
