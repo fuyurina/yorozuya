ffb90649475c4c5c7aff18439374605b1d1a5f36
Yorozuya <yorozuya@Yorozuya-Mac.local>
2024-12-27 15:40:51 +0700

    geag

--- app/(dashboard)/dashboard/Dashboard.tsx
+++ app/(dashboard)/dashboard/Dashboard.tsx
@@ -5,15 +5,12 @@ import { useDashboard } from '@/app/hooks/useDashboard'
 import { OrdersSummary } from './Summary'
 import { OrdersDetailTable } from './TableOrder'
 
-
-
-
 export function Dashboard() {
   const { orders, summary } = useDashboard()
 
   return (
     <Suspense fallback={<div>Memuat data...</div>}>
-      <div className="m-2">
+      <div className="overflow-auto h-[calc(100vh-4rem)]">
         <OrdersSummary summary={summary} />
         <OrdersDetailTable orders={orders} />
       </div>
